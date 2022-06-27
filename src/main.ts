0a5f5ad2f1b91c4c280dab5c6be8a8cdbaf5f756
Babbili <babbili@protonmail.com>
2022-06-27 07:20:14 +0400

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // will validate Requests to match types defined
+    })
+  )
+  await app.listen(PORT | 3000);
 }
 bootstrap();
